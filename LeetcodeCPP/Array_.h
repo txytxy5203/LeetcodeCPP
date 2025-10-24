919c28e819b1ea61e4ebcd21c5849bb15494a87f
学有 谈 <168973291@qq.com>
2025-10-24 21:31:11 +0800

    search

--- LeetcodeCPP/Array_.h
+++ LeetcodeCPP/Array_.h
@@ -173,7 +173,23 @@ public:
         {
             return {-1, -1};
         }
-
         return { first, second };
+    }
+    int search(vector<int>& nums, int target) 
+    {
+        // https://leetcode.cn/problems/binary-search/description/
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        if (left == nums.size() || nums[left] != target)
+            return -1;
+        return left;
     }
 };
