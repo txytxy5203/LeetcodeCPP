b8d31c189a6ffcb088b6e3493805b24c98191965
学有 谈 <168973291@qq.com>
2025-12-08 19:01:00 +0800

    isSameTree

--- LeetcodeCPP/TreeNode_.h
+++ LeetcodeCPP/TreeNode_.h
@@ -15,11 +15,24 @@ class TreeNode_
 {
 	int maxDepth(TreeNode* root) {
 		// https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
+		// Recur 还是老套路哈  基础情况 -> 要信息 -> 整合
 		if (root == nullptr)
 			return 0;
 		int l = maxDepth(root->left);
 		int r = maxDepth(root->right);
 		return std::max(l, r) + 1;
 	}	
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		// https://leetcode.cn/problems/same-tree/description/
+		if (p == nullptr && q == nullptr)
+			return true;
+		if (p == nullptr && q != nullptr || p != nullptr && q == nullptr)
+			return false;
+		if (p->val != q->val)
+			return false;
+		bool l = isSameTree(p->left, q->left);
+		bool r = isSameTree(p->right, q->right);
+		return l && r;
+	}
 };
 
